672ec6c64a37d7af9e011003ccc9edbb5fa00323
Sin-Dong-Won <56111933+Sin-Dong-Won@users.noreply.github.com>
2021-10-14 18:18:49 +0900

    2 Two File
    curve는 수정한 버전
    curve_repeatedly는 반복 부분만 뗀 버전

--- curve_repeatedly.py
+++ curve_repeatedly.py
@@ -60,7 +60,7 @@ def choose_next_points():
 
     return points
 
-def draw_elemently_4_points():
+def draw_repeatedly_4_points():
     global index
     global p1, p2, p3, p4
 
@@ -76,44 +76,6 @@ def draw_elemently_4_points():
         dx = ((-t**3 + 2*t**2 - t)*p1[0] + (3*t**3 - 5*t**2 + 2)*p2[0] + (-3*t**3 + 4*t**2 + t)*p3[0] + (t**3 - t**2)*p4[0])/2
         dy = ((-t**3 + 2*t**2 - t)*p1[1] + (3*t**3 - 5*t**2 + 2)*p2[1] + (-3*t**3 + 4*t**2 + t)*p3[1] + (t**3 - t**2)*p4[1])/2
         draw_point((dx, dy))
-
-def draw_curve_4_points(p1, p2, p3, p4):
-    draw_big_point(p1)
-    draw_big_point(p2)
-    draw_big_point(p3)
-    draw_big_point(p4)
-
-    # # draw p1-p2
-    # for i in range(0, 50, 1):
-    #     t = i / 100
-    #     x = (2*t**2-3*t+1)*p1[0]+(-4*t**2+4*t)*p2[0]+(2*t**2-t)*p3[0]
-    #     y = (2*t**2-3*t+1)*p1[1]+(-4*t**2+4*t)*p2[1]+(2*t**2-t)*p3[1]
-    #     draw_point((x, y))
-    # draw_point(p2)
-    #
-    # # draw p2-p3
-    # for i in range(0, 100, 1):
-    #     t = i / 100
-    #     x = ((-t**3 + 2*t**2 - t)*p1[0] + (3*t**3 - 5*t**2 + 2)*p2[0] + (-3*t**3 + 4*t**2 + t)*p3[0] + (t**3 - t**2)*p4[0])/2
-    #     y = ((-t**3 + 2*t**2 - t)*p1[1] + (3*t**3 - 5*t**2 + 2)*p2[1] + (-3*t**3 + 4*t**2 + t)*p3[1] + (t**3 - t**2)*p4[1])/2
-    #     draw_point((x, y))
-    # draw_point(p3)
-    #
-    # # draw p3-p4
-    # for i in range(50, 100, 1):
-    #     t = i / 100
-    #     x = (2*t**2-3*t+1)*p2[0]+(-4*t**2+4*t)*p3[0]+(2*t**2-t)*p4[0]
-    #     y = (2*t**2-3*t+1)*p2[1]+(-4*t**2+4*t)*p3[1]+(2*t**2-t)*p4[1]
-    #     draw_point((x, y))
-    # draw_point(p4)
-
-    # draw p4-p1
-    for i in range(0, 100, 1):
-        t = i / 100
-        x = ((-t**3 + 2*t**2 - t)*p3[0] + (3*t**3 - 5*t**2 + 2)*p4[0] + (-3*t**3 + 4*t**2 + t)*p1[0] + (t**3 - t**2)*p2[0])/2
-        y = ((-t**3 + 2*t**2 - t)*p3[1] + (3*t**3 - 5*t**2 + 2)*p4[1] + (-3*t**3 + 4*t**2 + t)*p1[1] + (t**3 - t**2)*p2[1])/2
-        draw_point((x, y))
-
 
 p1 = (100, 100); p2 = (-50, 400); p3 = (-200, -200); p4 = (400, -50)
 running = True
@@ -131,6 +93,6 @@ index = -1
 prepare_turtle_canvas()
 
 while running:
-    draw_elemently_4_points()
+    draw_repeatedly_4_points()
 
 turtle.done()
